09d39ae0903a71d3a4e608e746c4a81f698d13b5
Yuki Takei <yuki@weseek.co.jp>
2018-12-17 20:48:09 +0900

    correspond bump-version.sh to nocdn/Dockerfile

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sed -i -e "s/^\(ENV APP_VERSION v\).\+$/\1${RELEASE_VERSION}/" Dockerfile
+sed -i -e "s/^\(ENV APP_VERSION v\).\+$/\1${RELEASE_VERSION}/" nocdn/Dockerfile
 sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.2\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${RELEASE_VERSION}\2\3${RELEASE_VERSION}\4/" README.md
