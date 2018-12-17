dbc1f247dbf88ab5f9f4804af7cd5c7f254c06f9
Yuki Takei <yuki@weseek.co.jp>
2018-12-17 20:53:00 +0900

    modify bump-version.sh

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -2,4 +2,5 @@
 
 sed -i -e "s/^\(ENV APP_VERSION v\).\+$/\1${RELEASE_VERSION}/" Dockerfile
 sed -i -e "s/^\(ENV APP_VERSION v\).\+$/\1${RELEASE_VERSION}/" nocdn/Dockerfile
-sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.2\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${RELEASE_VERSION}\2\3${RELEASE_VERSION}\4/" README.md
+sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.3\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${RELEASE_VERSION}\2\3${RELEASE_VERSION}\4/" README.md
+sed -i -e "s/^\([*] \[\`\)[^\`]\+\(\`, \`3\.3-nocdn\`, .\+\]\)\(.\+\/blob\/v\).\+\(\/Dockerfile.\+\)$/\1${RELEASE_VERSION}-nocdn\2\3${RELEASE_VERSION}\4/" README.md
