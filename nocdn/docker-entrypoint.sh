2a68a10e3ea98a2b7b65e84f24d586be73f459db
Yuki Takei <yuki@weseek.co.jp>
2018-12-18 17:54:39 +0900

    duplicate docker-entrypoint.sh for nocdn

---
+++ nocdn/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+# Corresponds to `FILE_UPLOAD=local`
+mkdir -p /data/uploads
+if [ ! -e "$APP_DIR/public/uploads" ]; then
+  ln -s /data/uploads $APP_DIR/public/uploads
+fi
+
+exec "$@"
